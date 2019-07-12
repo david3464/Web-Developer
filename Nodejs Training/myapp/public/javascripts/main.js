73efe21289d59b52561df3bc0d8f4e72d2cc6d51
david3464 <51375293+david3464@users.noreply.github.com>
2019-07-12 19:47:40 -0400

    1

---
+++ Nodejs Training/myapp/public/javascripts/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    template: "<h1>Hi</h1>"
+    //data: {
+     // todos: [
+        //{ text: 'Learn JavaScript' },
+        //{ text: 'Learn Vue.js' },
+       // { text: 'Build Something Awesome' }
+     // ]
+    //}
+  })
